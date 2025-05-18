b5352fe931ee4da31a55c2053f4720affb508893
Robin Uhrich <robin.uhrich@gmail.com>
2025-05-18 17:49:47 +0200

    adapt collate fn for ChessDataset

--- project/utils/collate.py
+++ project/utils/collate.py
@@ -6,9 +6,9 @@ from torch import Tensor
 def collate_fn_next_token(batch: List[Tuple[Tensor, Tensor]]) -> Tuple[Tensor, Tensor]:
     # batch: list of tuples (word_tensor, tag_tensor)
     # Get input sentences
-    moves = [item[0] for item in batch]
+    moves = [item[0][0] for item in batch]
     # Get labels
-    next_move = [item[1] for item in batch]
+    next_move = [item[1][0] for item in batch]
     # Get maximum length in the batch
     lengths = [len(s) for s in moves]
     max_len = max(lengths)
@@ -20,9 +20,7 @@ def collate_fn_next_token(batch: List[Tuple[Tensor, Tensor]]) -> Tuple[Tensor, T
         pad_len = max_len - len(m)
         # Padding uses index 0 both for words and labels
         # NOTE: HARD CODED TO 1960 REPLACE WITH VOCAB SIZE
-        padded_moves.append(torch.cat([m, 1960 * torch.ones(pad_len, dtype=torch.int)]))
-        padded_next_move.append(
-            torch.cat([n_m, 1960 * torch.ones(pad_len, dtype=torch.int)])
-        )
+        padded_moves.append(torch.cat([m, torch.zeros(pad_len, dtype=torch.int)]))
+        padded_next_move.append(torch.cat([n_m, torch.zeros(pad_len, dtype=torch.int)]))
 
     return torch.stack(padded_moves), torch.stack(padded_next_move)
