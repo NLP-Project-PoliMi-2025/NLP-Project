f681e6804e4f354315a7c7591b9d66bca01c002f
Robin Uhrich <robin.uhrich@gmail.com>
2025-05-19 10:06:05 +0200

    added tb logger

--- project/scripts/train_game_annotation.py
+++ project/scripts/train_game_annotation.py
@@ -1,15 +1,17 @@
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
-from pytorch_lightning.loggers import CSVLogger
+from pytorch_lightning.loggers import CSVLogger, TensorBoardLogger
 from project.models.lit_modules import (
     SeqAnnotator,
 )  # or wherever the model is defined
 from project.dataset.lit_module import NextTokenDM, SeqAnnotationDM
+import torch
 
 
 def train(
     label: str, model_type="rnn", max_epochs=10, lr=1e-3, checkpoint_dir="checkpoints"
 ):
+    torch.set_float32_matmul_precision("medium")
     # Get datamodule
     dm = SeqAnnotationDM(
         "data/games_0001/train_100K.parquet",
@@ -26,19 +28,19 @@ def train(
     model = SeqAnnotator(
         n_target_classes=dm.get_num_labels()[0],
         vocab_size=dm.get_vocab_size()[0],
-        d_model=256,
+        d_model=512,
         n_layers=2,
         n_heads=4,
         dropout=0.1,
-        lr=lr,      
+        lr=lr,
         model_type=model_type,
         ignore_index=0,
-        freeze_embeddings=True, 
+        freeze_embeddings=True,
         word2vec="model_weights/word2vec.model"
     )
     print(dm.get_vocab_size())
     print(model)
-    
+
     # Checkpointing and logging
     checkpoint_callback = ModelCheckpoint(
         monitor="val_loss",
@@ -48,13 +50,13 @@ def train(
         filename=f"{model_type}-best",
     )
 
-    logger = CSVLogger("logs", name=model_type)
-
+    csv_logger = CSVLogger("csv_logs", name=model_type)
+    tb_logger = TensorBoardLogger("tb_logs", name=model_type)
     # Trainer
     trainer = Trainer(
         max_epochs=max_epochs,
         callbacks=[checkpoint_callback],
-        logger=logger,
+        logger=[csv_logger, tb_logger],
         accelerator="auto",
         devices="auto",
     )
