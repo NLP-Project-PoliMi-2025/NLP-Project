332fb03731c67dff97cabe6d9f422bd305c10dda
Paolo Ginefra <ginefra.paolo@gmail.com>
2025-05-24 16:11:16 +0200

    feat: add dataset from huggingface integration

--- project/dataset/lit_module.py
+++ project/dataset/lit_module.py
@@ -13,6 +13,8 @@ from torch.utils.data import DataLoader
 import sqlite3
 from project.dataset.ChessDataset import ChessDataset
 
+from datasets import Dataset
+
 
 class NextTokenDM(LightningDataModule):
     def __init__(
@@ -108,12 +110,13 @@ class NextTokenDM(LightningDataModule):
 class SeqAnnotationDM(LightningDataModule):
     def __init__(
         self,
-        train_file: str,
-        val_file: str,
-        test_file: str,
         batch_size: int,
         input_column: str,
         label_column: str,
+        datasets: Dataset = None,
+        train_file: str = "",
+        val_file: str = "",
+        test_file: str = "",
         num_worker: int = 1,
     ):
         super().__init__()
@@ -125,6 +128,8 @@ class SeqAnnotationDM(LightningDataModule):
         self.input_column = input_column
         self.label_column = label_column
         self.num_worker = num_worker
+
+        self.datasets: Dataset = datasets
 
         self.fit_set: ChessDataset = None
         self.validate_set: ChessDataset = None
@@ -146,22 +151,25 @@ class SeqAnnotationDM(LightningDataModule):
         if self.fit_set is not None:
             return
         self.fit_set = ChessDataset(
-            self.fit_file,
-            self.input_column,
-            self.label_column,
-            None,
+            dataset=self.datasets['train'] if self.datasets else None,
+            parquette_path=self.fit_file,
+            inputColumns=[self.input_column],
+            labelColumns=[self.label_column],
+            lookupReference=None,
         )
         self.validate_set = ChessDataset(
-            self.validate_file,
-            self.input_column,
-            self.label_column,
-            self.fit_set,
+            dataset=self.datasets['val'] if self.datasets else None,
+            parquette_path=self.validate_file,
+            inputColumns=[self.input_column],
+            labelColumns=[self.label_column],
+            lookupReference=self.fit_set,
         )
         self.test_set = ChessDataset(
-            self.test_file,
-            self.input_column,
-            self.label_column,
-            self.validate_set,
+            dataset=self.datasets['test'] if self.datasets else None,
+            parquette_path=self.test_file,
+            inputColumns=[self.input_column],
+            labelColumns=[self.label_column],
+            lookupReference=self.fit_set,
         )
         self.lookUps = self.test_set.lookup_tables
 
